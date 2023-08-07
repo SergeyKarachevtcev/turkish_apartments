545d7a08c35f86c3a44f2c121eda896cc1b6b380
SergeyKarachevtcev <estel141081@gmail.com>
2023-08-07 16:44:13 +0300

    fonal opdetes

--- js/script.js
+++ js/script.js
@@ -23,3 +23,37 @@ var swiper = new Swiper(".mySwiper", {
 	},
 });
 
+//map
+
+// Функция ymaps.ready() будет вызвана, когда
+// загрузятся все компоненты API, а также когда будет готово DOM-дерево.
+ymaps.ready(init);
+function init() {
+	// Создание карты.
+	var myMap = new ymaps.Map("map", {
+		// Координаты центра карты.
+		// Порядок по умолчанию: «широта, долгота».
+		// Чтобы не определять координаты центра карты вручную,
+		// воспользуйтесь инструментом Определение координат.
+		center: [36.548172, 32.018867],
+		// Уровень масштабирования. Допустимые значения:
+		// от 0 (весь мир) до 19.
+		zoom: 18,
+	});
+
+	//marck
+	var myPlacemark = new ymaps.Placemark(
+		[36.548172, 32.018867],
+		{},
+		{
+			iconLayout: "default#image",
+			iconImageHref: "./img/map/map_marck.png",
+			iconImageSize: [110, 110],
+			iconImageOffset: [-55, -55],
+		}
+	);
+
+	// Размещение геообъекта на карте.
+	myMap.geoObjects.add(myPlacemark);
+	myMap.behaviors.disable("scrollZoom");// отключил скролл карты 
+}
